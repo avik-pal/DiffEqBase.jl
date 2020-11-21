45d1462d675041a84db88fb275481a5fc11336e1
Avik Pal <avikpal@iitk.ac.in>
2020-11-21 20:16:35 +0530

    Remove function inlining for calculate_residuals

--- src/calculate_residuals.jl
+++ src/calculate_residuals.jl
@@ -6,12 +6,12 @@ Calculate element-wise residuals
 \\frac{ũ}{α+\\max{|u₀|,|u₁|}*ρ}
 ```
 """
-@inline @muladd function calculate_residuals(ũ::Number, u₀::Number, u₁::Number,
+@muladd function calculate_residuals(ũ::Number, u₀::Number, u₁::Number,
                                     α, ρ, internalnorm,t)
     ũ / (α + max(internalnorm(u₀,t), internalnorm(u₁,t)) * ρ)
 end
 
-@inline function calculate_residuals(ũ::Array{T}, u₀::Array{T}, u₁::Array{T}, α::T2,
+function calculate_residuals(ũ::Array{T}, u₀::Array{T}, u₁::Array{T}, α::T2,
                                              ρ::Real, internalnorm,t) where
                                              {T<:Number,T2<:Number}
     out = similar(ũ)
@@ -19,7 +19,7 @@ end
     out
 end
 
-@inline function calculate_residuals(ũ, u₀, u₁, α, ρ, internalnorm,t)
+function calculate_residuals(ũ, u₀, u₁, α, ρ, internalnorm,t)
   @.. calculate_residuals(ũ, u₀, u₁, α, ρ, internalnorm,t)
 end
 
@@ -32,12 +32,12 @@ Calculate element-wise residuals
 ```
 """
 
-@inline @muladd function calculate_residuals(u₀::Number, u₁::Number,
+@muladd function calculate_residuals(u₀::Number, u₁::Number,
                                     α, ρ, internalnorm,t)
     (u₁ - u₀) / (α + max(internalnorm(u₀,t), internalnorm(u₁,t)) * ρ)
 end
 
-@inline function calculate_residuals(u₀::Array{T}, u₁::Array{T}, α::T2,
+function calculate_residuals(u₀::Array{T}, u₁::Array{T}, α::T2,
                                              ρ::Real, internalnorm,t) where
                                              {T<:Number,T2<:Number}
     out = similar(u₀)
@@ -45,7 +45,7 @@ end
     out
 end
 
-@inline function calculate_residuals(u₀, u₁, α, ρ, internalnorm,t)
+function calculate_residuals(u₀, u₁, α, ρ, internalnorm,t)
   @.. calculate_residuals(u₀, u₁, α, ρ, internalnorm,t)
 end
 
@@ -58,12 +58,12 @@ Return element-wise residuals
 \\frac{δ E₁ + E₂}{α+\\max{scalarnorm(u₀),scalarnorm(u₁)}*ρ}.
 ```
 """
-@inline @muladd function calculate_residuals(E₁::Number, E₂::Number, u₀::Number, u₁::Number,
+@muladd function calculate_residuals(E₁::Number, E₂::Number, u₀::Number, u₁::Number,
                                              α::Real, ρ::Real, δ::Number, scalarnorm, t)
     (δ * E₁ + E₂) / (α + max(scalarnorm(u₀,t), scalarnorm(u₁,t)) * ρ)
 end
 
-@inline function calculate_residuals(E₁::Array{<:Number}, E₂::Array{<:Number},
+function calculate_residuals(E₁::Array{<:Number}, E₂::Array{<:Number},
                                      u₀::Array{<:Number}, u₁::Array{<:Number}, α::Real,
                                      ρ::Real, δ::Number, scalarnorm, t)
     out = similar(u₀)
@@ -71,7 +71,7 @@ end
     out
 end
 
-@inline function calculate_residuals(E₁, E₂, u₀, u₁, α, ρ, δ, scalarnorm, t)
+function calculate_residuals(E₁, E₂, u₀, u₁, α, ρ, δ, scalarnorm, t)
   @.. calculate_residuals(E₁, E₂, u₀, u₁, α, ρ, δ, scalarnorm, t)
 end
 
@@ -86,7 +86,7 @@ Save element-wise residuals
 ```
 in `out`.
 """
-@inline function calculate_residuals!(out, ũ, u₀, u₁, α, ρ, internalnorm,t)
+function calculate_residuals!(out, ũ, u₀, u₁, α, ρ, internalnorm,t)
   @.. out = calculate_residuals(ũ, u₀, u₁, α, ρ, internalnorm,t)
   nothing
 end
@@ -101,7 +101,7 @@ Save element-wise residuals
 ```
 in `out`.
 """
-@inline function calculate_residuals!(out, u₀, u₁, α, ρ, internalnorm,t)
+function calculate_residuals!(out, u₀, u₁, α, ρ, internalnorm,t)
   @.. out = calculate_residuals(u₀, u₁, α, ρ, internalnorm,t)
 end
 
@@ -113,7 +113,7 @@ Calculate element-wise residuals
 \\frac{δ E₁ + E₂}{α+\\max{scalarnorm(u₀),scalarnorm(u₁)}*ρ}.
 ```
 """
-@inline function calculate_residuals!(out, E₁, E₂, u₀, u₁, α, ρ, δ, scalarnorm, t)
+function calculate_residuals!(out, E₁, E₂, u₀, u₁, α, ρ, δ, scalarnorm, t)
   @.. out = calculate_residuals(E₁, E₂, u₀, u₁, α, ρ, δ, scalarnorm, t)
   out
 end
